48d32ec22989d9831230ff2af76fab60676b4cef
Nuno Anselmo <n.anselmo@hotmail.com>
2014-10-08 14:24:28 +0100

    Atribuídas tarefas

--- projecto.py
+++ projecto.py
@@ -1,18 +1,21 @@
 # Grupo 21: Nuno Anselmo (81900); Fabio Pinheiro (81909); Mariana Silva (81938)
 
 def verifica_cc(num_cc):
+    #FABIO
     #Input: int(num_cc)
     #Output 1: string(cat_em), string(rede_em)
     #Output 2: string('cartao invalido')
     print("!!!!!")
 
 def gera_num_cc(rede_em):
+    #NUNO
     #Input: string(rede_em)
     #Output 1: int(num_cc)
     #Output 2: string('emissor invalido')
     print("!!!!!")
 
 def calc_soma(a_somar):
+    #NUNO
     #Input: string(a_somar)
     #Output: int(soma)
     soma = 0
@@ -22,33 +25,39 @@ def calc_soma(a_somar):
     return soma
 
 def luhn_verifica(a_verificar):
+    #MARIANA
     #Input: string(a_verificar)
     #Output: bool(valido)
     print("!!!!!")
 
 def comeca_por():
+    #FABIO
     #Input: string(), string()
     #Output: bool()
     print("!!!!!")
     
 def comeca_por_um():
+    #FABIO
     #Input: string(), tuple()
     #Output: bool()
     print("!!!!!")
     
 def valida_iin():
+    #NUNO
     #Input: string()
     #Output 1: string()
     #Output 2: string('')
     print("!!!!!")
 
 def categoria():
+    #MARIANA
     #Input: string()
     #Output 1: string(cat_em)
     #Output 2: string('categoria invalida')
     print("!!!!!")
     
 def digito_verificacao():
+    #MARIANA
     #Input: string()
     #Output: string(dig_ver)
     print("!!!!!")
